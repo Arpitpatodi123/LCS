41a580dea0f820c220161413a0fd641e87d93f3d
Girjesh <girjeshlt@gmail.com>
2019-07-31 11:16:09 +0530

    first Commit

---
+++ src/test/java/com/probity/LCS/LcsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.probity.LCS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LcsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
